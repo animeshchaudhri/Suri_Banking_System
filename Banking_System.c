ea56340119dd8b45fac4d415f0d83bafd9587642
akash1979 <akashsuri1979@gmail.com>
2024-09-02 18:35:46 +0530

    made the basic login and sign in using file handling and will know work toadd more features like withdrawal balance and such more thing

--- Banking_System.c
+++ Banking_System.c
@@ -2,12 +2,34 @@
 #include<string.h>
 #include <stdlib.h>
 #include <time.h>
+#include<stdbool.h>
 #include "encrypt.h"
 typedef struct account{
-    char userName[50];
-    char password[50];
+    char userName[60];
+    char password[60];
     int balance;
 }account;
+bool checkuser(char* userName,char* getpasw){
+    FILE* fptr=fopen("user.txt","r");
+    char usertemp[60];
+    if(fptr!=NULL){
+        while(fscanf(fptr,"%s %s",usertemp,getpasw)!=EOF){
+            if(strcmp(usertemp,userName)==0){
+                fclose(fptr);
+                return true;
+            }
+        }
+    }
+    fclose(fptr);
+    return false;
+}
+void adduser(char* userName,char* password){
+    FILE* fptr=fopen("user.txt","a");
+    if(fptr!=NULL){
+        fprintf(fptr,"%s %s\n",userName,password);
+    }
+    fclose(fptr);
+}
 int main(){
     int choice=1;
     printf("Welcome to Suri Banking system\n");
@@ -20,12 +42,42 @@ int main(){
         printf("3 To Exit\n");
         printf("Enter Your Choice: ");
         scanf("%d",&choice);
+        char userName[60];
+        char password[60];
         if(choice==1){
-            printf("hello\n");
             sleep(1);
+            printf("Hello user Please Enter The Following Details: \n");
+            printf("UserName: ");
+            scanf("%s",userName);
+            printf("Password: ");
+            scanf("%s",password);
+            sleep(1);
+            char getpasw[60];
+            if(checkuser(userName,getpasw)){
+                sleep(1);
+                if(strcmp(password,getpasw)==0){
+                    printf("Login successFull\n");
+                }else{
+                    printf("wrong password\n");
+                }
+            }else{
+                sleep(1);
+                printf("User Doesn't Exsist\n");
+            }
         }else if(choice==2){
-            printf("bye\n");
             sleep(1);
+            printf("Enter the following Details to add User\n");
+            printf("UserName: ");
+            scanf("%s",userName);
+            char getpasw[60];
+            if(checkuser(userName,getpasw)){
+                sleep(1);
+                printf("Sorry user already Exist\n");
+            }else{
+                printf("Password: ");
+                scanf("%s",password);
+                adduser(userName,password);
+            }
         }
     }while(choice!=3);
     return 0;
