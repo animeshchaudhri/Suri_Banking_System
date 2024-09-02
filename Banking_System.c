92b53cf1db194790f659c3eb2924f60d744ea4c5
akash1979 <akashsuri1979@gmail.com>
2024-09-03 01:09:32 +0530

    added basic functionalities like addbalance,removebalance,withdraw,transfer still needed to add log based system

--- Banking_System.c
+++ Banking_System.c
@@ -6,14 +6,13 @@
 #include "encrypt.h"
 typedef struct account{
     char userName[60];
-    char password[60];
     int balance;
 }account;
 bool checkuser(char* userName,char* getpasw){
     FILE* fptr=fopen("user.txt","r");
     char usertemp[60];
     if(fptr!=NULL){
-        while(fscanf(fptr,"%s %s",usertemp,getpasw)!=EOF){
+        while(fscanf(fptr,"%s %s %d",usertemp,getpasw)!=EOF){
             if(strcmp(usertemp,userName)==0){
                 fclose(fptr);
                 return true;
@@ -26,9 +25,68 @@ bool checkuser(char* userName,char* getpasw){
 void adduser(char* userName,char* password){
     FILE* fptr=fopen("user.txt","a");
     if(fptr!=NULL){
-        fprintf(fptr,"%s %s\n",userName,password);
+        fprintf(fptr,"%s %s %d\n",userName,password,0);
     }
     fclose(fptr);
+}
+int getBalance(char* userName){
+    FILE* fptr=fopen("user.txt","r");
+    int balance=0;
+    char usertemp[60];
+    char password[60];
+    if(fptr!=NULL){
+        while(fscanf(fptr,"%s %s %d",usertemp,password,&balance)!=EOF){
+            if(strcmp(usertemp,userName)==0){
+                return balance;
+            }
+        }
+    }
+    return balance;
+}
+void addbalance(char* userName,int amount){
+    FILE* fptr1=fopen("user.txt","r");
+    FILE* fptr2=fopen("temp.txt","w");
+    char* usertemp[60];
+    char* password[60];
+    int balance=0;
+    if(fptr1!=NULL && fptr2!=NULL){
+        while(fscanf(fptr1,"%s %s %d",usertemp,password,balance)!=EOF){
+            if(strcmp(userName,usertemp)==0){
+                fprintf(fptr2,"%s %s %d\n",usertemp,password,balance+amount);
+            }else{
+                fprintf(fptr2,"%s %s %d\n",usertemp,password,balance);
+            }
+        }
+    }
+    fclose(fptr1);
+    fclose(fptr2);
+    remove("user.txt");
+    rename("temp.txt","user.txt");
+}
+void withdraw(char* userName,int amount){
+    FILE* fptr1=fopen("user.txt","r");
+    FILE* fptr2=fopen("temp.txt","w");
+    char* usertemp[60];
+    char* password[60];
+    int balance=0;
+    if(fptr1!=NULL && fptr2!=NULL){
+        while(fscanf(fptr1,"%s %s %d",usertemp,password,balance)!=EOF){
+            if(strcmp(userName,usertemp)==0){
+                if(amount>balance){
+                    printf("Insufficent Balance\n");
+                    fprintf(fptr2,"%s %s %d\n",usertemp,password,balance);
+                }else{
+                    fprintf(fptr2,"%s %s %d\n",usertemp,password,balance-amount);
+                }
+            }else{
+                fprintf(fptr2,"%s %s %d\n",usertemp,password,balance);
+            }
+        }
+    }
+    fclose(fptr1);
+    fclose(fptr2);
+    remove("user.txt");
+    rename("temp.txt","user.txt");
 }
 int main(){
     int choice=1;
@@ -56,7 +114,34 @@ int main(){
             if(checkuser(userName,getpasw)){
                 sleep(1);
                 if(strcmp(password,getpasw)==0){
-                    printf("Login successFull\n");
+                    sleep(2);
+                    int choice2=1;
+                    do{
+                        printf("Login SuccessFull For %s\n",userName);
+                        printf("Tell which operation you want to perform :\n");
+                        printf("1 to add Balance\n");
+                        printf("2 to Withdraw\n ");
+                        printf("3 to Transfer\n");
+                        printf("4 to Check Balance\n");
+                        printf("5 to Exit\n");
+                        scanf("%d",&choice2);
+                        if(choice2==1){
+                            int amount;
+                            printf("Please Enter Amount: ");
+                            scanf("%d",&amount);
+                            addbalance(userName,amount);
+                        }else if(choice2==2){
+                            int amount;
+                            printf("Please Enter Amount: ");
+                            scanf("%d",&amount);
+                            withdraw(userName,amount);
+                        }else if(choice==3){
+
+
+                        }else if(choice==4){
+                            printf("The balance of the %s on %s : %d\n",userName,__DATE__,getBalance(userName));
+                        }
+                    }while(choice2!=5);
                 }else{
                     printf("wrong password\n");
                 }
